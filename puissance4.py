f4652f0538f1cf0ad366073a15cd274c8d8c7078
Suyan SUBRAMANIAM <suyan.subramaniam@ens.uvsq.fr>
2022-04-12 11:36:43 +0200

    ajouter nom

--- puissance4.py
+++ puissance4.py
@@ -1,6 +1,7 @@
 #############################
 GROUPE TD 3 
-BOUDINAR LOUNES 22106882 
+BOUDINAR LOUNES 22106882
+SUBRAMANIAM SUYAN 22107171 
 
 https://github.com/uvsq22106882/puissance4.git
 ############################
